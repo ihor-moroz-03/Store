769b6e92db176762abd63d74eed5d6ff285b4a4d
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-12-08 09:30:02 +0200

    refactor in IProduct

--- Products/IProduct.cs
+++ Products/IProduct.cs
@@ -5,16 +5,10 @@ namespace Store.Products
 {
     public interface IProduct : IEquatable<IProduct>, IEnumerable<IDetail>
     {
-        //string this[string detail] { get; }
-
         string Name { get; }
 
         double Price { get; }
 
         string Category { get; }
-
-        //bool HasDetail(string detail);
-
-        //bool TryGetDetail(string detail, out string value);
     }
 }
